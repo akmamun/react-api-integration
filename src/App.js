79aeda60a2e433f19402007f2a1e475d89c438f4
mrxmamun <almamunkhan04@gmail.com>
2019-04-05 13:07:44 +0600

    implements fetch and axios get method

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from "react-router-dom";
+// import MyForm from "./components/forms/MyForm";
+import API from "./components/Api";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+            <Switch>
+          <Route path="/" component={API} exact />
+{/* 
+          <Route path="/form" component={MyForm} exact /> */}
+          </Switch>
+      </React.Fragment>
+
     );
   }
 }
